73c27f5781e71ad299592baaac633f82dfbbfe85
Danny Liehr <dannyisyelling@gmail.com>
2019-01-31 15:50:52 -0500

    first commit

---
+++ liehr_danny.py
@@ -0,0 +1,11 @@
+class Engine(object):
+    def __init__ (self):
+        pass
+
+class Entity(object):
+    def __init__ (self):
+        pass
+
+class Player(Entity):
+    def create (self):
+        pass
